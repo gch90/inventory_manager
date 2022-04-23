4ff6da6e575a959855f8f32183c51a48513c4211
Georges Chami <g.chami90@gmail.com>
2022-04-23 12:04:45 -0400

    created the items controller and views, added simple_form gem

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,51 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def edit; end
+
+  def show
+    @shipments = Shipment.where(status: 0)
+    @shipped_item = ShippedItem.new
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:quantity, :name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "items#index"
+
+  resources :items
+  resources :shipments, only: [:create, :show, :index, :update]
+  resources :shipped_items, only: [:create]
 end
