d07305d284251df54690936775847273440947a5
Georges Chami <g.chami90@gmail.com>
2022-04-23 13:35:16 -0400

    completed shipments controller and views and styled a bit the application for better naviagation

---
+++ app/controllers/shipments_controller.rb
@@ -0,0 +1,32 @@
+class ShipmentsController < ApplicationController
+  before_action :set_shipment, only: [:show, :update]
+
+  def index
+    @shipments = Shipment.all
+  end
+
+  def create
+    @shipment = Shipment.new
+
+    if @shipment.save
+      redirect_to shipments_path
+    else
+      render :shipments_path, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @shipped_items = @shipment.shipped_items
+  end
+
+  def update
+    @shipment.status = 1
+    if @shipment.save
+      redirect_to shipment_path(@shipment)
+    else
+      render shipment_path(@shipment)
+      render :edit, status: :unprocessable_entity
+
+    end
+  end
+end
