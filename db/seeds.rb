7cc8d5f8bc9761662efdae608fcd389b2bf3f803
Georges Chami <g.chami90@gmail.com>
2022-04-24 20:59:53 -0400

    completed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ShippedItem.delete_all
+Item.delete_all
+Shipment.delete_all
+
+p 'running seed'
+items = []
+5.times do
+  items << (Item.create!(name: Faker::Appliance.unique.equipment, quantity: 10))
+end
+
+shipments = []
+3.times do
+  shipments << Shipment.create!
+end
+
+items[0..1].each do |item|
+  ShippedItem.create(item: item, quantity: 5, shipment: shipments[0])
+end
+
+items.each do |item|
+  ShippedItem.create(item: item, quantity: 1, shipment: shipments[1])
+end
+
+shipments[0].shipped!
+
+Faker::Appliance.unique.clear
+
+p 'seeding complete'
